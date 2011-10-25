6550a8799d40a82a6b93cbcf7238711962cbf1c7
mheld <heldgeek@gmail.com>
2011-10-24 22:22:03 -0400

    added android project to repo

---
+++ android/src/com/emma/Emma.java
@@ -0,0 +1,13 @@
+package com.emma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Emma extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
